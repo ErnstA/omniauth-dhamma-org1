457ecf4a1c072d2b5f0580e6cece63c50b1f3068
Ernst Arnold <ernst@eka.one>
2021-03-28 23:23:37 +1100

    start copying analogous to omniauth-google-oauth2 gem

--- lib/omniauth-dhamma-org1.rb
+++
@@ -1,6 +0,0 @@
-require "omniauth-dhamma-org1/version"
-
-module OmniauthDhammaOrg1
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/omniauth-dhamma-org1/version.rb
+++
@@ -1,3 +0,0 @@
-module OmniauthDhammaOrg1
-  VERSION = "0.1.1"
-end
